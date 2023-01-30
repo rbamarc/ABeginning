8fbeccbf9e8bd94c4e72cbf01e20db586a147f36
rbamarc <rbamarc929@gmail.com>
2023-01-29 20:59:30 -0800

    first commit

--- src/main/java/netbeans/abeginning/ABeginning.java
+++ src/main/java/netbeans/abeginning/ABeginning.java
@@ -13,5 +13,9 @@ public class ABeginning {
         System.out.println("typing code is easier than I thought!");
         System.out.println("Typity typity type");
         System.out.println("after I finish typing,");
+        System.out.println("I'll compile my code");
+        System.out.println("And then when I run it,");
+        System.out.println("Then console will print out all of my brillant words!");
+        System.out.println("and it all started with \'Hello world!\' ..");
     }
 }
