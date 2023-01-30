ee9bec217302a43b96ca235ca8cf10a4ff63b968
rbamarc <rbamarc929@gmail.com>
2023-01-29 20:47:48 -0800

    first commit

---
+++ src/main/java/netbeans/abeginning/ABeginning.java
@@ -0,0 +1,17 @@
+
+package netbeans.abeginning;
+
+/**
+ *
+ * @author halfrikan
+ */
+public class ABeginning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello from the Guild");
+        System.out.println("typing code is easier than I thought!");
+        System.out.println("Typity typity type");
+        System.out.println("after I finish typing,");
+    }
+}
